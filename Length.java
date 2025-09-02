467510aa7080eaa2c5fec98b620e848571e533f1
P Kaveri <kaverikaverihy@gmail.com>
2025-09-02 20:38:07 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Length{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string");
+        String a=sc.nextLine();
+        System.out.println("The length of string is:" +a.length());
+    }
+}
