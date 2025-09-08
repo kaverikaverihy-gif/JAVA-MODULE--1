c3299e660f70f3e8687bcc03331ecdb30a5d45bc
P Kaveri <kaverikaverihy@gmail.com>
2025-09-08 18:14:36 +0530

    Add files via upload

---
+++ Or.java
@@ -0,0 +1,14 @@
+public class Or{
+    public static void main(String[]args){
+        int a=12;
+        int b=10;
+        int result=a|b;
+        System.out.println("a|b="+result);
+    }
+    {
+        int a=12;
+        int b=10;
+        int result=a^b;
+        System.out.println("a^b="+result);
+    }
+}
