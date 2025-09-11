9dc71e8b41ced62212a1194bd4ce0fa07b018ebb
P Kaveri <kaverikaverihy@gmail.com>
2025-09-11 21:17:59 +0530

    Add files via upload

---
+++ greaterlesser.java
@@ -0,0 +1,13 @@
+public class greaterlesser{
+    public static void main(String[]args){
+        int x=15,y=10;
+        int a=10,b=10;
+        int e=4,f=12;
+        int k=14,s=03;
+        System.out.println("x>y:"+(x>y));
+        System.out.println("a<b:"+(a<b));
+        System.out.println("e<=f:"+(e<=f));
+        System.out.println("k>=s:"+(k>=s));
+
+    }
+}
