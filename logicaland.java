6300b257fcd8afcdbea9d4bad60f7af7534fe9e6
P Kaveri <kaverikaverihy@gmail.com>
2025-09-11 21:18:26 +0530

    Add files via upload

---
+++ logicaland.java
@@ -0,0 +1,12 @@
+public class logicaland{
+    public static void main(String[]args){
+        int age=25;
+        boolean haslicense=true;
+        if(age>18&&haslicense){
+            System.out.println("Eligible to drive.");
+    }else{
+        System.out.println("not eligible to drive.");
+    }
+
+}
+}
