870f4f67458d191b08b0c19a584827aa3e77fad9
P Kaveri <kaverikaverihy@gmail.com>
2025-09-23 18:18:52 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,8 @@
+class Foreach {
+    public static void main(String[] args) {
+        String[]fruits={"apple","banana","cherry"};
+        for(String fruit:fruits){
+        System.out.println("Fruit:"+fruit);
+    }
+}
+}
