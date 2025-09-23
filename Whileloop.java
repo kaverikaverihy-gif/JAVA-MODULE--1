1c36c5ac7e3e1af2fc3f8ed4ea80d2edb37dac7f
P Kaveri <kaverikaverihy@gmail.com>
2025-09-23 18:13:20 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+public class Whileloop{
+    public static void main(String[]args){
+        int number=12345;
+        int sum=0;
+        while(number>0){
+            int digit=number%10;
+            sum+=digit;
+            number=number/10;
+            
+        }
+        System.out.println("Sum of digits="+sum);
+    }
+}
