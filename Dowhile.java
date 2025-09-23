719deb308e6bd4b7e07b7c2668633c4743e19311
P Kaveri <kaverikaverihy@gmail.com>
2025-09-23 18:16:46 +0530

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,9 @@
+class Dowhile {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("Value:"+i);
+            i++;
+        }while(i<=5);
+    }
+}
